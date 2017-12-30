e158b9a0fdc34bfcc65021f14e1a3e47509ff50f
mrtgbenson <34960383+mrtgbenson@users.noreply.github.com>
2017-12-30 14:21:13 +0000

    Create brussels.py

---
+++ brussels.py
@@ -0,0 +1,14 @@
+brussels = bool()
+response = str()
+
+response = input("Do you like Brussel sprouts?")
+if response == "yes":
+  brussels == True
+  print("Well done!")
+  
+elif response == "no":
+  brussels == False
+  print("Well you should because they are tasty!")
+  
+else:
+  print("Not sure? Give them another try!")
