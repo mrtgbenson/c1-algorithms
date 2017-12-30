eab3e0363031a91d2eff049fa829e723b5814ec3
mrtgbenson <34960383+mrtgbenson@users.noreply.github.com>
2017-12-30 12:16:42 +0000

    Create number-magic.py

---
+++ number-magic.py
@@ -0,0 +1,28 @@
+"""Number magic version 0.1"""
+
+my_number=int() #declare an integer variable
+your_number=int()
+
+your_number=int(input("Please enter a whole number: ")
+my_number=0 #sets and initial value of 0
+
+my_number=your_number
+my_number=your_number*3
+print("Your number multiplied by 3 is : ")
+print(my_number) #Displays the result on the console
+
+my_number=my_number+6
+print("Now we add 6: ")
+print(my_number)
+
+my_number=my_number/3
+print("Divide by 3 ....")
+print(my_number)
+
+my_number=my_number-your_number
+print("Now subtract the number that you gave me..."
+print("And the number is......")
+print(my_number)
+
+
+
